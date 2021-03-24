8b330f40673ae08262a1d0c9c871ff4db03987d2
frodo821 <sakaic2003@gmail.com>
2021-03-24 14:04:09 +0900

    キーボードを別コンポーネントに分離

--- src/screens/Keyboard.svelte
+++ src/screens/Keyboard.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-  import { Card, Select } from 'svelte-materialify';
+  import { Card, Select, Switch } from 'svelte-materialify';
   import { connector } from '../connector';
+  import SopranoKeyboard from '../widgets/SopranoKeyboard.svelte';
+  import AltoKeyboard from '../widgets/AltoKeyboard.svelte';
 
   const onPointerDown = (
     evt: PointerEvent & { currentTarget: HTMLDivElement }
@@ -8,6 +10,8 @@
     const target = evt.currentTarget;
     target.style.background = 'rgb(79, 232, 255)';
     const key = parseInt(target.getAttribute('data-keyid'));
+
+    console.log(key);
 
     if (key !== key || typeof value !== 'string') {
       return;
@@ -20,9 +24,8 @@
   ) => {
     const target = evt.currentTarget;
     target.style.background = '';
-    const key = parseInt(target.getAttribute('data-keyid'));
 
-    if (key !== key || typeof value !== 'string') {
+    if (typeof value !== 'string') {
       return;
     }
 
@@ -31,6 +34,7 @@
 
   let items: { name: string; value: string }[] = [];
   let value: any;
+  let keyboard: boolean = true;
 
   connector.get_status().then((it) => {
     items = it.connectedPorts.map(({ port: name }) => ({ name, value: name }));
@@ -38,320 +42,19 @@
 </script>
 
 <div class="screen">
-  <div class="ports">
+  <div class="controls">
     <Card style="padding: 0.5rem;margin: 1rem 0.5rem;">
       <Select bind:value {items}>Port</Select>
+      <Switch bind:checked={keyboard}
+        >キーボード: {keyboard ? 'ソプラノ' : 'アルト'}</Switch
+      >
     </Card>
   </div>
   <Card style="padding: 0.5rem;margin: 1rem 0.5rem;">
-    <div class="keyboard">
-      <div class="white-keys">
-        <div
-          class="key wh-key"
-          data-keyid="01"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="03"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="05"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="06"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="08"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="10"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="12"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="13"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="15"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="17"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="18"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="20"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="22"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="24"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="25"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key wh-key"
-          data-keyid="27"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-      </div>
-      <div class="black-keys">
-        <div
-          class="key bl-key"
-          data-keyid="02"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key bl-key"
-          data-keyid="04"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key bl-key"
-          data-keyid="07"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key bl-key"
-          data-keyid="09"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key bl-key"
-          data-keyid="11"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key bl-key"
-          data-keyid="14"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key bl-key"
-          data-keyid="16"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key bl-key"
-          data-keyid="19"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key bl-key"
-          data-keyid="21"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key bl-key"
-          data-keyid="23"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-        <div
-          class="key bl-key"
-          data-keyid="26"
-          role="button"
-          on:pointerdown={onPointerDown}
-          on:pointerup={onPointerUp}
-          on:pointerleave={onPointerUp}
-        />
-      </div>
-    </div>
+    {#if keyboard}
+      <SopranoKeyboard {onPointerDown} {onPointerUp} />
+    {:else}
+      <AltoKeyboard {onPointerDown} {onPointerUp} />
+    {/if}
   </Card>
 </div>
-
-<style lang="scss">
-  @import '../styles/utils';
-  .keyboard {
-    position: relative;
-
-    @include mobile {
-      margin-left: 2rem;
-    }
-
-    @include desktop {
-      width: 48rem;
-      height: 10rem;
-      margin: auto;
-    }
-
-    .key {
-      cursor: pointer;
-      border: solid 1px gray;
-    }
-
-    .black-keys {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      pointer-events: none;
-    }
-
-    .bl-key {
-      background: black;
-      pointer-events: all;
-
-      @include mobile {
-        margin: 1rem 0;
-        width: 8rem;
-        height: 2rem;
-
-        &:first-child {
-          margin-top: 2rem;
-        }
-
-        &:nth-child(5n + 3) {
-          margin-top: 4rem;
-        }
-
-        &:nth-child(5n + 1):not(:first-child) {
-          margin-top: 4rem;
-        }
-      }
-
-      @include desktop {
-        margin: 0 0.5rem;
-        width: 2rem;
-        height: 8rem;
-        float: left;
-        &:first-child {
-          margin-left: 2rem;
-        }
-
-        &:nth-child(5n + 3) {
-          margin-left: 3.5rem;
-        }
-
-        &:nth-child(5n + 1):not(:first-child) {
-          margin-left: 3.5rem;
-        }
-      }
-    }
-
-    .wh-key {
-      background: white;
-
-      @include mobile {
-        width: 10rem;
-        height: 3rem;
-      }
-
-      @include desktop {
-        width: 3rem;
-        height: 10rem;
-        float: left;
-      }
-    }
-  }
-</style>
---
+++ src/widgets/SopranoKeyboard.svelte
@@ -0,0 +1,318 @@
+<script lang="ts">
+  export let onPointerDown: (
+    evt: PointerEvent & { currentTarget: HTMLDivElement }
+  ) => void;
+  export let onPointerUp: (
+    evt: PointerEvent & { currentTarget: HTMLDivElement }
+  ) => void;
+</script>
+
+<div class="keyboard">
+  <div class="white-keys">
+    <div
+      class="key wh-key"
+      data-keyid="01"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="03"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="05"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="06"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="08"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="10"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="12"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="13"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="15"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="17"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="18"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="20"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="22"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="24"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="25"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key wh-key"
+      data-keyid="27"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+  </div>
+  <div class="black-keys">
+    <div
+      class="key bl-key"
+      data-keyid="02"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key bl-key"
+      data-keyid="04"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key bl-key"
+      data-keyid="07"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key bl-key"
+      data-keyid="09"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key bl-key"
+      data-keyid="11"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key bl-key"
+      data-keyid="14"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key bl-key"
+      data-keyid="16"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key bl-key"
+      data-keyid="19"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key bl-key"
+      data-keyid="21"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key bl-key"
+      data-keyid="23"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+    <div
+      class="key bl-key"
+      data-keyid="26"
+      role="button"
+      on:pointerdown={onPointerDown}
+      on:pointerup={onPointerUp}
+      on:pointerleave={onPointerUp}
+    />
+  </div>
+</div>
+
+<style lang="scss">
+  @import '../styles/utils';
+  .keyboard {
+    position: relative;
+
+    @include mobile {
+      margin-left: 2rem;
+    }
+
+    @include desktop {
+      width: 48rem;
+      height: 10rem;
+      margin: auto;
+    }
+
+    .key {
+      cursor: pointer;
+      border: solid 1px gray;
+    }
+
+    .black-keys {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      pointer-events: none;
+    }
+
+    .bl-key {
+      background: black;
+      pointer-events: all;
+
+      @include mobile {
+        margin: 1rem 0;
+        width: 8rem;
+        height: 2rem;
+
+        &:first-child {
+          margin-top: 2rem;
+        }
+
+        &:nth-child(5n + 3) {
+          margin-top: 4rem;
+        }
+
+        &:nth-child(5n + 1):not(:first-child) {
+          margin-top: 4rem;
+        }
+      }
+
+      @include desktop {
+        margin: 0 0.5rem;
+        width: 2rem;
+        height: 8rem;
+        float: left;
+        &:first-child {
+          margin-left: 2rem;
+        }
+
+        &:nth-child(5n + 3) {
+          margin-left: 3.5rem;
+        }
+
+        &:nth-child(5n + 1):not(:first-child) {
+          margin-left: 3.5rem;
+        }
+      }
+    }
+
+    .wh-key {
+      background: white;
+
+      @include mobile {
+        width: 10rem;
+        height: 3rem;
+      }
+
+      @include desktop {
+        width: 3rem;
+        height: 10rem;
+        float: left;
+      }
+    }
+  }
+</style>
